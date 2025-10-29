24baf1f6bc6bbc32da2c56585ab8730908ee83ff
Witherfly <withiwither@gmail.com>
2025-10-28 23:20:01 -0500

    added build_db_url helper and use it for database URL construction

--- notebooks/connection.py
+++ notebooks/connection.py
@@ -5,18 +5,21 @@ def load_config(config_path):
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
 
+def build_db_url(config):
+    return (
+        f"{config['drivername']}://{config['user']}:{config['password']}@"
+        f"{config['host']}:{config['port']}/{config['dbname']}"
+    )
+
 def connect(schema):
     config = load_config('../config_fill.yml')
     config_co = config['CO_SA']
     config_etl = config['ETL_PRO']
     config_etl_or = config['ETL_PRO_OR']
 
-    url_co = (f"{config_co['drivername']}://{config_co['user']}:{config_co['password']}@{config_co['host']}:"
-              f"{config_co['port']}/{config_co['dbname']}")
-    url_etl = (f"{config_etl['drivername']}://{config_etl['user']}:{config_etl['password']}@{config_etl['host']}:"
-               f"{config_etl['port']}/{config_etl['dbname']}")
-    url_etl_or = (f"{config_etl_or['drivername']}://{config_etl_or['user']}:{config_etl_or['password']}@{config_etl_or['host']}:"
-                  f"{config_etl_or['port']}/{config_etl_or['dbname']}")
+    url_co = build_db_url(config_co)
+    url_etl = build_db_url(config_etl)
+    url_etl_or = build_db_url(config_etl_or)
 
     co_oltp = create_engine(url_co)
     etl_conn = create_engine(url_etl)
